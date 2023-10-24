a31b5babdc27b6f276b107c42182bb2a647043ff
igorlage <igor-rl@hotmail.com>
2023-10-24 09:14:46 -0300

    chore

--- quick_install.sh
+++ quick_install.sh
@@ -8,4 +8,26 @@ mkdir -p scripts/deploy_droid
 git clone https://github.com/igor-rl/ddroid.git
 mv ddroid/scripts/deploy_droid/* scripts/deploy_droid
 rm -rf ddroid
-bash ./scripts/deploy_droid/install.sh
+add_to_config() {
+  local config_file=$1
+  echo "alias ddroid='./scripts/deploy_droid/ddroid.sh'" >> $config_file
+  echo "Alias 'ddroid' adicionado ao $config_file."
+  echo
+  echo
+  echo "🤖 DDroid instalado com sucesso!"
+  sleep 1
+  echo
+  echo
+  echo "Para iniciar o '🤖 DDroid', execute o comando 'ddroid' no seu terminal."
+  exec $SHELL
+}
+echo "Iniciando a instalação do 🤖 DDroid no seu projeto..."
+sleep 4
+if [[ $SHELL == *"zsh"* ]]; then
+    add_to_config ~/.zshrc
+elif [[ $SHELL == *"bash"* ]]; then
+    add_to_config ~/.bashrc
+else
+    echo "Shell não reconhecido. O alias não foi adicionado automaticamente."
+fi
+exec $SHELL
